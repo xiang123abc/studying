ceca93ec9f86dae08293d7530e79d84f25a56f1f
xiangabc <1032673923@qq.com>
2019-05-14 12:28:50 +0800

    面经

--- src/cn/ecut/demo/Demo1.java
+++ src/cn/ecut/demo/Demo1.java
@@ -1,53 +1,32 @@
 package cn.ecut.demo;
 
-import java.util.Scanner;
- 
+import java.util.*;
 public class Demo1{
- 
-    public static void main(String args[]){
- 
-        Scanner sc=new Scanner(System.in);
- 
-        while(sc.hasNext()){
- 
-            String s=sc.next();
- 
-            String str=s.substring(1,s.length()-1);
- 
-            String ss[]=str.split(",");
- 
-            int a[]=new int[ss.length];
- 
-            for(int i=0;i<ss.length;i++)
- 
-            a[i]=Integer.parseInt(ss[i]);
- 
-            boolean o=false;
- 
-            int count=0;
- 
-            int k=0;
- 
-            while(count<=a.length){
- 
-                count++;
- 
-                k+=a[k];
- 
-                if(k<0||k>=a.length){
- 
-                    o=true;
- 
-                    break;
- 
-                }
- 
+    public static void main(String args []){
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()){
+            String str = scan.nextLine();
+            String[]  strs = str.split(",");
+            String[] temparr = strs[0].split(" ");
+            int [] arr = new int [temparr.length];
+            int N = Integer.parseInt(strs[1]);
+            for(int i=0;i<temparr.length;i++){
+                arr[i]= Integer.parseInt(temparr[i]);
             }
- 
-            System.out.println(o);
- 
+            if(arr.length<3){
+                System.out.print("False");
+                return;
+            }
+             for(int i=0;i<arr.length-2;i++){
+            	 for(int j=0;j<arr.length-1;j++){
+            		 for(int k=0;k<arr.length;k++) {
+            			 if(arr[i]+arr[k]+arr[j]==N) {
+            				 System.out.print("True");
+            				 return ;
+            			 }
+            		 }
+            	 }
+             }
         }
- 
     }
- 
 }
