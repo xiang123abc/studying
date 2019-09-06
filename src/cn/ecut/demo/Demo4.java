d7457c192c11c0d8e8832f4a0e7b4d7603b62871
xiangabc <1032673923@qq.com>
2019-09-06 23:31:23 +0800

    面经

--- src/cn/ecut/demo/Demo4.java
+++ src/cn/ecut/demo/Demo4.java
@@ -1,38 +1,21 @@
 package cn.ecut.demo;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Demo4 {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int  n = scan.nextInt();
-		int[] fav = new int[n];
-		for(int i=0;i<n;i++) {
-			fav[i] = scan.nextInt();
+		ArrayList arr= new ArrayList();
+		for(int i=0;i<17;i++) {
+			arr.add("a");
 		}
-		Map<Integer,List<Integer>> map = new HashMap();
-		for(int i=0;i<n;i++) {
-			int key = fav[i];
-			int value = i+1;
-			if(!map.containsKey(key)) {
-				List<Integer> list = new LinkedList();
-				list.add(value);
-				map.put(key,list);
-			}else {
-				List list = map.get(key);
-				list.add(value);
-				
-			}
-			int m = scan.nextInt();
-			List list = new LinkedList();
-			for(int i=0;i<m;i++){
-				int lo=scan.nextInt();
-	            int hi=scan.nextInt();
-	            int des=scan.nextInt();
-	            List<Integer> list2=map.get(des);
-	            int count=0;
-			}
-		}
+		int n = 7;
+		n = n>>1;
+		System.out.println(n);
+		HashMap map = new HashMap();
+		map.put("", 1);
+		ConcurrentHashMap cch = new ConcurrentHashMap();
+		cch.put("", 1);
 	}
 }
